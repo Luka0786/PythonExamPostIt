56971c1e29ca6a6fb3f4316c7a4df5d59600bc18
AndreasJepsenNielsen <andr921c@stud.kea.dk>
2019-05-14 16:01:06 +0200

    Hallo Mand

--- PostIt/accounts/models.py
+++ PostIt/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
