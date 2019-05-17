8d5836cd5f364838798fab7d2543374f049dba6a
Lukas <luka0786@stud.kea.dk>
2019-05-17 16:00:02 +0200

    virker

--- PostIt/PostItApp/urls.py
+++ PostIt/PostItApp/urls.py
@@ -9,6 +9,7 @@ app_name = 'postitapp'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('posts', views.posts, name='posts'),
-    #path('/posts/new', views.create_post, name="create_post"),
-    path('/posts/yours', views.your_posts, name="your_posts"),
+    path('posts/create', views.create_post, name="create_post"),
+    path('posts/yours', views.your_posts, name="your_posts"),
+    path('posts/post', views.post, name="post"),
 ]
--- PostIt/PostItApp/views.py
+++ PostIt/PostItApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.urls import reverse
 
 from .models import PostModel
 
@@ -18,3 +20,42 @@ def your_posts(request):
             'your_posts': your_posts
         }
         return render(request, 'your_posts.html', context)
+
+def post(request):
+    if request.method == 'GET':
+        context = {
+            'post': request.post
+        }
+        return render(request, 'post.html', context)
+
+def create_post(request):
+    if request.method == 'GET':
+        return render(request, 'create_post.html')
+    
+    if request.method == 'POST':
+        post = PostModel()
+        post.user = request.user
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.save()
+        return HttpResponseRedirect(reverse('postitapp:home'))
+    
+    return HttpResponseBadRequest()
+
+"""def create_comment(request):
+    if request.method == 'GET':
+        return render(request, 'todoapp/new.html')
+    
+    if request.method == 'POST':
+        todo = Todo()
+        todo.user = request.user
+        todo.text = request.POST['text']
+        completed = request.POST.getlist('completed')
+        if len(completed) > 0:
+            todo.completed = True
+        else:
+            todo.completed = False
+        todo.save()
+        return HttpResponseRedirect(reverse('todoapp:index'))
+    
+    return HttpResponseBadRequest()"""
