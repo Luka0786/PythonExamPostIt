d657027256be4cfb28a9bd8d4d1f2a0aec9dcfd9
Lukas <luka0786@stud.kea.dk>
2019-05-17 14:53:12 +0200

    post and your post get works

--- PostIt/PostItApp/urls.py
+++ PostIt/PostItApp/urls.py
@@ -8,4 +8,7 @@ app_name = 'postitapp'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('posts', views.posts, name='posts'),
+    #path('/posts/new', views.create_post, name="create_post"),
+    path('/posts/yours', views.your_posts, name="your_posts"),
 ]
--- PostIt/PostItApp/views.py
+++ PostIt/PostItApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from .models import PostModel
+
 # Create your views here.
+def posts(request):
+    if request.method == 'GET':
+        posts = PostModel.objects.all()
+        context = {
+            'posts': posts
+        }
+        return render(request, 'posts.html', context)
+
+def your_posts(request):
+    if request.method =='GET':
+        your_posts = PostModel.objects.filter(user=request.user)
+        context = {
+            'your_posts': your_posts
+        }
+        return render(request, 'your_posts.html', context)
